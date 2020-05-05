7fd1322ec43a0e7834d3915588391f9f40e8c7dc
Pawel Bozemski <paw.boz@gmail.com>
2020-05-05 14:37:19 +0200

    Add navbar to header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 // import { connect } from 'react-redux';
 // import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
 
@@ -8,17 +10,19 @@ import styles from './Header.module.scss';
 
 const Component = () => (
   <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <Button className={styles.header_button} href="https://google.com" variant="outline-primary" size="lg">
-      Login with Google
-      </Button>
-      <Button className={styles.header_button} href="/" variant="outline-info" size="lg">
-      My ads
-      </Button>
-      <Button className={styles.header_button} href="/" variant="outline-danger" size="lg">
-      Logout
-      </Button>
-    </div>
+    <Navbar expand="lg" variant="light" bg="light">
+      <Container className={styles.wrapper}>
+        <Button className={styles.header_button} href="https://google.com" variant="outline-primary" size="lg">
+            Login with Google
+        </Button>
+        <Button className={styles.header_button} href="/" variant="outline-info" size="lg">
+            My ads
+        </Button>
+        <Button className={styles.header_button} href="/" variant="outline-danger" size="lg">
+            Logout
+        </Button>
+      </Container>
+    </Navbar>
   </div>
 );
 
