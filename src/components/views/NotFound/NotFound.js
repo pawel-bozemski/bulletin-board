38dcde1182af930456365a362af6d15ddc8ecba7
Pawel Bozemski <paw.boz@gmail.com>
2020-05-05 14:31:04 +0200

    Change fix notfound page

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import clsx from 'clsx';
 import styles from './NotFound.module.scss';
+import Button from 'react-bootstrap/Button';
 
-const Component = ({className}) => (
-  <div className={clsx(className, styles.root)}>
-    <Typography className={styles.root}>
-      <h2>Nothing to do here</h2>
-      <Button>
-        <Link href="/">
-          Go back to the homepage
-        </Link>
-      </Button>
-    </Typography>
+const Component = () => (
+  <div className={styles.root}>
+    <h2>Nothing to do here</h2>
+    <Button variant="danger"href="/">
+      Go back to the homepage
+    </Button>
   </div>
 );
 
